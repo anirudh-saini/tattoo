b2b5b3d47073734d97b96d23a98888dd54a55f37
rohan <rohanverma031@gmail.com>
2024-02-20 12:23:05 +0530

    saini bhaii ka jalwa haii

--- src/app/compound/components/Card.tsx
+++ src/app/compound/components/Card.tsx
@@ -2,18 +2,29 @@ import React from "react";
 import styles from "./card.module.scss";
 
 interface CardProps {
-    data: {
-        heading: string;
-        img: string;
-        data: string;
-    };
+	data: {
+		heading: string;
+		img: string;
+		text: string;
+	};
 }
 
 export default function Card({ data }: CardProps) {
-    return (
-        <div className={styles.card}>
-            <div className=""></div>
-            <div></div>
-        </div>
-    );
+	return (
+		<div className={styles.card}>
+			<div className={styles.visual}>
+				<img src={data.img} alt={data.heading} />
+
+				<div className={styles.heading}>
+					<h3>{data.heading}</h3>
+				</div>
+			</div>
+
+			<div className={styles.info}>
+				<h3>{data.heading}</h3>
+
+				<p>{data.text}</p>
+			</div>
+		</div>
+	);
 }
